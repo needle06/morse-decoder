b20cae74f8cf9d1d3a502b2516f4bdb124ba1881
Valeria Kanonik <needle0606@gmail.com>
2019-10-02 22:06:30 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let decodedString = '';
+    for (let i = 0; i < expr.length; i+=10) {
+        let char1 = expr.slice(i, i + 10);
+        decodedString += decodeOneCharacter(char1)
+    }
+    return decodedString;
+}
+
+
+function decodeOneCharacter (char) {
+    if (char == "**********") return " ";    
+    let trimmedChar = char.split('00').join('');
+    newChar = translateFromZeroesAndOnesToDotsAndDashes(trimmedChar);
+    let decodedChar = MORSE_TABLE[newChar];
+    return decodedChar;
+}
+
+// 11 -> "-", 10-> ".", "1011" -> ".-"
+function translateFromZeroesAndOnesToDotsAndDashes (char) {
+    let translatedChar = "";
+    for (let i = 0; i < char.length; i+=2) {
+        let dividedOnTwoSymbols = char.slice(i, i + 2);
+        if (dividedOnTwoSymbols == "11") translatedChar += "-"
+        if (dividedOnTwoSymbols == "10") translatedChar += "."
+    }
+    return translatedChar;
 }
 
 module.exports = {
